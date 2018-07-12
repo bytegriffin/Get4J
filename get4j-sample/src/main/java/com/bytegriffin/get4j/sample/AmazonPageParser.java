2e1671d2c9a6b9b69aa0c80055e20e5653cb8af2
bytegriffin <bytegriffin@126.com>
2018-07-12 16:19:27 +0800

    fix

--- get4j-sample/src/main/java/com/bytegriffin/get4j/sample/AmazonPageParser.java
+++ get4j-sample/src/main/java/com/bytegriffin/get4j/sample/AmazonPageParser.java
@@ -9,16 +9,16 @@ import com.bytegriffin.get4j.parse.PageParser;
  */
 public class AmazonPageParser implements PageParser {
 
-    @Override
-    public void parse(Page page) {
-       System.err.println("书名"+page.getTitle() + "  图书地址: " + page.getUrl() );
-    }
+	@Override
+	public void parse(Page page) {
+		//利用Jsoup解析详情页面中的html元素
+		String time = page.jsoupText("span.a-size-medium.a-color-secondary.a-text-normal");
+		System.err.println("书名" + page.getTitle() + "  图书地址: " + page.getUrl() + " 出版时间：" + time);
+	}
 
-    public static void main(String[] args) throws Exception {
-        Spider.list_detail().fetchUrl("https://www.amazon.cn/gp/bestsellers/books/ref=zg_bs_books_pg_1?ie=UTF8&pg={1}")
-        		.detailSelector("div.a-fixed-left-grid-col.a-col-right > a.a-link-normal[href]")
-                .parser(AmazonPageParser.class)
-                .thread(1).start();
-    }
+	public static void main(String[] args) throws Exception {
+		Spider.list_detail().fetchUrl("https://www.amazon.cn/gp/bestsellers/books/ref=zg_bs_books_pg_1?ie=UTF8&pg={1}")
+				.detailSelector("a.a-link-normal[href]").parser(AmazonPageParser.class).thread(1).start();
+	}
 
 }
--- get4j-sample/src/main/java/com/bytegriffin/get4j/sample/CsdnBlogPageParser.java
+++ get4j-sample/src/main/java/com/bytegriffin/get4j/sample/CsdnBlogPageParser.java
@@ -8,12 +8,17 @@ public class CsdnBlogPageParser implements PageParser {
 
     @Override
     public void parse(Page page) {
-        System.err.println(page.getTitle() + "  " + page.getUrl());
+    	if(page.isListPage()){ //第一次返回的是列表页数据
+    		System.err.println(page.getUrl()+ "  " + page.json("$..channel"));
+    	} else { //之后返回的是详情页数据
+    		System.err.println(page.getUrl() + "  " +page.getDetailLinks());
+    	}
+        
     }
 
     public static void main(String[] args) throws Exception {//blog_list clearfix
-        Spider.list_detail().fetchUrl("http://blog.csdn.net/?&page={1}").sleepRange(3, 5)
-                .resourceSelector("img.head[src]").totalPages(1).detailSelector("h2.csdn-tracking-statistics>a[href]")
+        Spider.list_detail().fetchUrl("https://blog.csdn.net/api/articles?type=more&category=home&shown_offset=1531325255862708")
+                //.detailSelector("$.articles[*].url")
                 .parser(CsdnBlogPageParser.class).defaultUserAgent()
                 //.jdbc("jdbc:mysql://localhost:3306/get4j?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8&user=root&password=root")
                 //.mongodb("mongodb://localhost:27017")
